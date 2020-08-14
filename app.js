8bb35943b65d6b8fc73e123daaeff2a97793a589
Anik Sarker <aniksarker1612@gmail.com>
2020-08-15 05:16:05 +0800

    refresh after 10 sec this feature added

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-async function fetchData(){
+async function fetchData() {
     try {
-    let response = await fetch('https://randomuser.me/api/'); 
-    let data = await response.json();
-    let user = data.results[0];
-    console.log(data);
-    const ui = new UI();
-    ui.displayUserInfo(user);
+        let response = await fetch('https://randomuser.me/api/?results=1');
+        let data = await response.json();
+        let user = data.results[0];
+        console.log(data);
+        const ui = new UI();
+        ui.displayUserInfo(user);
     } catch (error) {
         console.log(error);
     }
-   
+
 }
 fetchData();
 
 setInterval(fetchData, 10000);
 
+let sec = 10;
+function timer() {
+    sec--;
+    if (sec <= 0) {
+        sec = 10;
+    }
+    const timer = document.getElementById('timer');
+    timer.innerText = 'refresh after: ' + sec + ' seconds';
+}
+
+setInterval(timer, 1000);
 
